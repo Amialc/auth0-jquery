4fcc9ea58c35c3a8ac71e899e18bb23cf2c4bfaf
Vlad <pleo12@gmail.com>
2016-04-28 11:56:32 +0300

    deleted unnecessary code, reworked widget examples to use redirect mode against popup mode

--- examples/widget-with-thirdparty-api/app.js
+++ examples/widget-with-thirdparty-api/app.js
@@ -6,41 +6,47 @@ $(document).ready(function() {
 
     var userProfile;
 
+    var hash = lock.parseHash();
+    if (hash) {
+        if (hash.error) {
+            console.log("There was an error logging in", hash.error);
+        }
+        else {
+            lock.getProfile(hash.id_token, function (err, profile) {
+                if (err) {
+                    console.log('Cannot get user', err);
+                    return;
+                }
+                // Save the JWT token.
+                localStorage.setItem('id_token', hash.id_token);
+                var idToken = hash.id_token;
+                console.log("Auth0 token", idToken);
+                var delegationOptions = {
+                    id_token: idToken,
+                    api: API_TYPE
+                };
+                lock.getClient().getDelegationToken(delegationOptions,
+                    function(err, thirdPartyApiToken) {
+                        if (err) {
+                            console.log("There was an error getting a delegation token: " + JSON.stringify(err));
+                        } else {
+                            localStorage.setItem('thirdPartyApiToken', thirdPartyApiToken.id_token);
+                            console.log("Third party token", thirdPartyApiToken.id_token);
+                        }
+                    });
+                // Save the profile
+                userProfile = profile;
+
+                $('.login-box').hide();
+                $('.logged-in-box').show();
+                $('.name').text(profile.name);
+            });
+        }
+    }
+
     $('.btn-login').click(function(e) {
       e.preventDefault();
-      lock.showSignin(function(err, profile, token) {
-        if (err) {
-          // Error callback
-          console.log("There was an error");
-          alert("There was an error logging in");
-        } else {
-          console.log("Auth0 token", token);
-          // Success callback
-          var delegationOptions = {
-            id_token: token,
-            api: API_TYPE
-          };
-          lock.getClient().getDelegationToken(delegationOptions,
-            function(err, thirdPartyApiToken) {
-              if (err) {
-                console.log("There was an error getting a delegation token: " + JSON.stringify(err));
-              } else {
-                localStorage.setItem('thirdPartyApiToken', thirdPartyApiToken.id_token);
-                console.log("Third party token", thirdPartyApiToken.id_token);
-              }
-            });
-
-          // Save the JWT token.
-          localStorage.setItem('userToken', token);
-
-          // Save the profile
-          userProfile = profile;
-
-          $('.login-box').hide();
-          $('.logged-in-box').show();
-          $('.nickname').text(profile.nickname);
-        }
-      });
+      lock.show();
     });
 
 
